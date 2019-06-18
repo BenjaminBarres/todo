343b42d7db08f584fc33d29e9895046bd4faeb14
BenjaminBarres <51902053+BenjaminBarres@users.noreply.github.com>
2019-06-18 11:05:38 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
 
     handleClick() {
       this.setState({
-        counter: this.state.counter + 999999999999
+        counter: this.state.counter + 1
       });
   }
  render() {
