acef8484c8257ee2986d2ae585d80177cacde34c
Ruslan Absaliamov <secretive211@gmail.com>
2019-06-20 12:01:28 +0400

    1

--- src/Todo.js
+++ src/Todo.js
@@ -6,15 +6,29 @@ export class Todo extends React.Component {
         super(props);
 
         const { task } = props;
-        
+
         this.state = {
             task: task || {}
         };
     }
 
+    handleDelete() {
+        this.props.onDelete(this.state.task.id);
+    }
+
+    handleComplte() {
+        this.props.onComplete(this.state.task.id);
+    }
+
     render() {
         const { task } = this.state;
 
-        return <li className="todo">{task.text}</li>;
+        return <li className={"todo" + (task.completed ? " completed" : "")}>
+            <input onChange={this.handleComplte.bind(this)} type="checkbox"></input>
+            <div className="todo-text">
+                {task.text}
+            </div>
+            <i className="fas fa-trash" onClick={this.handleDelete.bind(this)}></i>
+        </li>;
     }
- }
+}
