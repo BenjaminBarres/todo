db415cf294648db1b1cd31cb007a26650cfbbac2
Ruslan Absaliamov <secretive211@gmail.com>
2019-06-28 11:20:02 +0400

    Доделал

---
+++ src/About.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export function About() {
+    return <div>
+        <h2>О программе</h2>
+        <p>Простая программа для управления списком дел</p>
+    </div>;
+}
--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './App.css';
 import { Todos } from './Todos';
+import { ModalContainer, ModalDialog } from 'react-modal-dialog';
+import { About } from './About';
 
-class App extends React.Component {
+ export default class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      isShowingModal: false
+    }
   }
+
+  handleClick() {
+    this.setState({
+      isShowingModal: true
+    });
+  } 
+
+  handleClose() {
+    this.setState({
+      isShowingModal: false
+    });
+  } 
+  
  render() {
   const tasks = [
     {  
@@ -28,10 +47,26 @@ class App extends React.Component {
 
   return (
     <div>
+      <div className="menu">
+        <h1>Simple ToDo List.</h1>
+        <div className="sep"></div>
+        <a href="#">Главная</a>
+        <a onClick={this.handleClick.bind(this)}>О программе</a>
+      </div>
      <Todos tasks={tasks}></Todos>
-    </div>
-  );
- }
+     <div className="footer">
+      Ruslan Absaliamov &copy;{(new Date()).getFullYear()}
+     </div>
+  {
+    this.state.isShowingModal &&
+    <ModalContainer onClose={this.handleClose.bind(this)}>
+    <ModalDialog onClose={this.handleClose.bind(this)}>
+      <About></About>
+    </ModalDialog>
+  </ModalContainer>
+  }
+</div>
+);
+} 
 }
 
-export default App;
